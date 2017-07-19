56d84209fb8977f4795f46c8167267f4c394bbbc
suhai yehuza <soyehuza@gmail.com>
2017-07-19 15:01:06 -0700

    generated a kokoveli database with a categories table

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -3,5 +3,5 @@ import ReactDOM from 'react-dom';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
-  ReactDOM.render( <h1 className='static_home'>Welcome to kokoveli</h1>, root);
+  ReactDOM.render( <h1 className='static_home'>welcome to kokoveli</h1>, root);
 });
