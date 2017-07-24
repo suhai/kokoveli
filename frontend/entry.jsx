cdfff837c525d74166a17055d47a9d6b40c50cc1
suhai yehuza <soyehuza@gmail.com>
2017-07-24 13:06:55 -0700

    updated placeholder text

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const placeHolder = (
+  <div>
+    <h1 className='static_home'> Welcome to kokoveli </h1>
+    <br />
+    <h2 className='static_home2'> This Is Still In The Development Stages </h2>
+    <h2 className='static_home2'> Please Check Back Soon </h2>
+  </div>
+);
+
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
-  ReactDOM.render( <h1 className='static_home'>welcome to kokoveli</h1>, root);
+  ReactDOM.render(placeHolder, root);
 });
